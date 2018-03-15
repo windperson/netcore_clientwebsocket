8226dc571fa3f1e00d390d5bafd8e6d592e388e4
Chen Yu Pao <windperson@gmail.com>
2018-03-15 19:47:41 +0800

    fix exit hangout, but will produce bad log on server side

--- Program.cs
+++ Program.cs
@@ -3,144 +3,148 @@ using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace netcore_websocket_client
-{
-    class Program
-    {
+namespace netcore_websocket_client {
+    class Program {
         const int bufferSize = 1024 * 4;
 
         static string webSocketUri = @"ws://localhost:5000/ws";
 
-        static void Main(string[] args)
-        {
-            using (var cts = new CancellationTokenSource())
-            {
+        static void Main (string[] args) {
+            Task doJob;
+            using (var cts = new CancellationTokenSource ()) {
                 var token = cts.Token;
 
-                var waiter = Connect(webSocketUri, token).ConfigureAwait(false).GetAwaiter();
+                doJob = Task.Run (async () => {
+                    try {
+                        await Connect (webSocketUri, cts).ConfigureAwait (false);
+                    } catch (OperationCanceledException) {
+                        Console.WriteLine ("Stoping all tasks");
+                    } catch (Exception ex) {
+                        Console.Error.WriteLine (ex);
+                    }
+                });
+
+                doJob.ContinueWith ((_) => {
+                    Console.WriteLine ("Press any key to exit.");
+                });
 
                 var stopKey = ConsoleKey.A;
-                Console.WriteLine("press \"" + stopKey + "\" to exit.");
-                while (!IsKeyPressed(stopKey))
-                {
+                Console.WriteLine ("press \"" + stopKey + "\" to exit.");
+                while (!IsKeyPressed (stopKey)) {
                     //loop 
-                    Thread.Sleep(new TimeSpan(0, 0, 1));
+                    Thread.Sleep (new TimeSpan (0, 0, 1));
                 }
-                cts.Cancel();
+                cts.Cancel ();
             }
-            Console.WriteLine("Ending program, press any key to exit.");
-            Console.ReadKey();
+            if (doJob != null) {
+                Console.WriteLine ("Ending program...");
+                doJob.Wait ();
+                Console.ReadKey ();
+            }
         }
 
-        static async Task Connect(string uri, CancellationToken token)
-        {
-            token.ThrowIfCancellationRequested();
+        static async Task Connect (string uri, CancellationTokenSource tokenSource) {
 
-            using (var webSocket = new ClientWebSocket())
-            {
-                try
-                {
-                    await webSocket.ConnectAsync(new Uri(uri), token);
-                    await Task.WhenAll(Send(webSocket, new TimeSpan(0, 0, 1), token), Receive(webSocket, token));
-                }
-                catch (OperationCanceledException)
-                {
-                    Console.WriteLine("cancell recv and send tasks...");
-                }
-                catch (AggregateException ex) when (ex.InnerException is TaskCanceledException)
-                {
-                    //do nothing
-                    Console.WriteLine("exit due to Task(s) canceled");
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine(ex);
-                }
-                finally
-                {
-                    Console.WriteLine("Closing connection...");
-                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "client initiate disconnect", CancellationToken.None);
+            using (var webSocket = new ClientWebSocket ()) {
+                try {
+
+                    await webSocket.ConnectAsync (new Uri (uri), tokenSource.Token);
+
+                    var sendTask = Task.Run (async () => {
+                        await Send (webSocket, new TimeSpan (0, 0, 1), tokenSource.Token);
+                    }, tokenSource.Token);
+
+                    var recvTask = Task.Run (async () => {
+                        await Receive (webSocket, tokenSource.Token);
+                    }, tokenSource.Token);
+
+                    await Task.WhenAll (sendTask, recvTask).ConfigureAwait (false);
+                } finally {
+                    Console.WriteLine ("Closing connection...");
+                    if (webSocket.State != WebSocketState.Aborted) {
+                        await webSocket.CloseAsync (WebSocketCloseStatus.NormalClosure, "client initiate disconnect", CancellationToken.None);
+                    }
                 }
             }
         }
 
         //send string periodically
-        static async Task Send(ClientWebSocket webSocket, TimeSpan period, CancellationToken token)
-        {
-            while (webSocket.State == WebSocketState.Open)
-            {
-                if (token.IsCancellationRequested)
-                {
-                    Console.WriteLine("stop sending...");
-                    token.ThrowIfCancellationRequested();
+        static async Task Send (ClientWebSocket webSocket, TimeSpan period, CancellationToken token) {
+            while (webSocket.State == WebSocketState.Open) {
+                if (token.IsCancellationRequested) {
+                    Console.WriteLine ("stop sending...");
+                    token.ThrowIfCancellationRequested ();
                 }
 
                 string sendStr = @"hello ClientWebSocket!";
-                try
-                {
-                    var sendBuffer = StringToByteArray(sendStr);
+                try {
+                    var sendBuffer = StringToByteArray (sendStr);
 
-                    await webSocket.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                    await webSocket.SendAsync (new ArraySegment<byte> (sendBuffer), WebSocketMessageType.Text, true, token);
+                } catch (OperationCanceledException) {
+                    Console.WriteLine ("cancel current sending operation...");
+                } catch (Exception ex) {
+                    Console.Error.WriteLine (ex);
                 }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine(ex);
-                }
-                await Task.Delay(period);
+                await Task.Delay (period);
             }
         }
 
-        static async Task Receive(ClientWebSocket webSocket, CancellationToken token)
-        {
-            var recvBuffer = new byte[bufferSize];
-            while (webSocket.State == WebSocketState.Open)
-            {
-                if (token.IsCancellationRequested)
-                {
-                    Console.WriteLine("stop receiving...");
-                    token.ThrowIfCancellationRequested();
+        static async Task Receive (ClientWebSocket webSocket, CancellationToken token) {
+            while (webSocket.State == WebSocketState.Open) {
+                if (token.IsCancellationRequested) {
+                    Console.WriteLine ("stop receiving...");
+                    token.ThrowIfCancellationRequested ();
                 }
 
-                try
-                {
-                    var resultResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(recvBuffer), CancellationToken.None);
+                try {
+                    var recvBuffer = new byte[bufferSize];
+                    var resultResult = await webSocket.ReceiveAsync (new ArraySegment<byte> (recvBuffer), token);
 
-                    if (resultResult.MessageType == WebSocketMessageType.Close)
-                    {
-                        Console.WriteLine("receive close connection request");
-                        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "server initiate closing", token);
+                    #region Remote Initiated Closing
+                    if (resultResult.MessageType == WebSocketMessageType.Close) {
+                        Console.WriteLine ("receive close connection request");
+                        await webSocket.CloseAsync (WebSocketCloseStatus.NormalClosure, "server initiate closing", token);
                         break;
                     }
+                    #endregion
 
-                    Console.WriteLine("Receive: " + GetReadableString(recvBuffer));
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine(ex);
+                    Console.WriteLine ("Receive: " + GetReadableString (recvBuffer));
+                } catch (OperationCanceledException) {
+                    Console.WriteLine ("cancel current receiving operation...");
+                    //await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,"client shutdown", CancellationToken.None);
+                } catch (Exception ex) {
+                    Console.Error.WriteLine (ex);
                 }
             }
+            Console.WriteLine ("end receiving...");
         }
 
         #region Util functions
 
-        static byte[] StringToByteArray(string input)
-        {
-            return System.Text.Encoding.Default.GetBytes(input);
+        static Task CreateLongRunningTask (Func<Task> worker, CancellationToken token) {
+            var task = Task.Factory.StartNew (
+                function : worker,
+                cancellationToken : token,
+                creationOptions : TaskCreationOptions.LongRunning,
+                scheduler : TaskScheduler.Default
+            );
+            return task.Unwrap ();
         }
 
-        static string GetReadableString(byte[] buffer)
-        {
-            var nullStart = Array.IndexOf(buffer, (byte)0);
+        static byte[] StringToByteArray (string input) {
+            return System.Text.Encoding.Default.GetBytes (input);
+        }
+
+        static string GetReadableString (byte[] buffer) {
+            var nullStart = Array.IndexOf (buffer, (byte) 0);
             nullStart = (nullStart == -1) ? buffer.Length : nullStart;
-            return System.Text.Encoding.Default.GetString(buffer, 0, nullStart);
+            return System.Text.Encoding.Default.GetString (buffer, 0, nullStart);
         }
 
-        static bool IsKeyPressed(ConsoleKey kbKey)
-        {
-            if (Console.KeyAvailable)
-            {
-                if (Console.ReadKey(true).Key == kbKey)
-                {
+        static bool IsKeyPressed (ConsoleKey kbKey) {
+            if (Console.KeyAvailable) {
+                if (Console.ReadKey (true).Key == kbKey) {
                     return true;
                 }
             }
